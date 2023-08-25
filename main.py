b2e6903d1a21c1c517f2b79069ef16763ecb9254
saljeshh <saljesh2020@gmail.com>
2023-08-25 11:47:01 +0545

    day 2

--- main.py
+++ main.py
@@ -1,21 +1,85 @@
-from fastapi import Body, FastAPI
+from typing import Optional
+from fastapi import FastAPI, HTTPException, Response, status
+from pydantic import BaseModel
+from fastapi.params import Body
+from random import randrange
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
+#  NOW lets do pydantic validation for body
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # default is true
+    rating: Optional[int] = None  # options field and default is none
 
 
-@app.get("/posts")
-async def get_posts():
-    return {"data": "This is your first post!"}
+my_posts = [
+    {"title": "Title of post 1", "content": "Content of post 1", "id": 1},
+    {"title": "Title of post 2", "content": "Content of post 2", "id": 2},
+]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 
 @app.post("/createposts")
 # extract all data from req body, type cast to dict and store in payload
-async def create_post(payload: dict = Body(...)):
+async def create_post(payload: dict = Body()):
     return {
         "message": f"You did {payload['type']} from {payload['account']} account on {payload['date']} amount of {payload['amount']}"
     }
+
+
+# pydantic automatically gets data for us
+@app.post("/pydantic/create_post")
+# post here is stored as pydantic model
+async def pydantic_create_post(post: Post):
+    print(post)
+    # convert pydantic model to dictionary
+    print(post.model_dump())
+
+    # for crud
+    post_dict = post.model_dump()
+    # add id field with random number of converted dict from pydantic model
+    post_dict["id"] = randrange(1, 1000000)
+    # add or append new data to our database ie. our array for now
+    my_posts.append(post_dict)
+
+    return {"message": "Successfully created"}
+
+
+# CRUD
+@app.get("/posts")
+def get_posts():
+    # Fast api automatically searialize the array to json
+    return {"data": my_posts}
+
+
+@app.get("/posts/latest")
+def get_latest_post():
+    latest = my_posts[len(my_posts) - 1]
+    return {"latest_post": latest}
+
+
+# id is path parameter
+@app.get("/posts/{id}")
+# we dont need to create response here if we throw HTTPException
+def get_post(id: int, response: Response):
+    post = find_post(id)
+
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"Error": f"post with id {id} was not found!"}
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            # here name of message must be detail
+            detail=f"post with id {id} was not found!",
+        )
+
+    return {"data": post}
--- readme.md
+++ readme.md
@@ -2,8 +2,24 @@
 
 - upto 1 hour 7 minute done
 - basics of fast api
+- in fastapi docs routes of node.js = path operations
 - get and post method + postman
 
 ## Screenshot of postman create_post api
 
 ![Create_Post](./postman_ss/day1.png)
+
+# Day 2 of FastAPI
+
+- BaseModel, Optional field
+- Creating my_posts array like a fake db
+- Learn about REST pattern
+- model.dump() replaced .dict() to convert pydantic model to py dictionary
+- using random number using randrange(initial,max)
+- Fast API automatically searialize array to json
+- path paramter and response status
+- HttpException
+
+## Screenshot of REST
+
+![Create_Post](./notes_ss/day2-i1.png)
